6708df1be552d1993d3a911ee554517144cc033f
Ludwig-Langell <Ludwig@Langell.nu>
2026-09-11 14:46:53 +0200

    Made Jump 10x as strong
    Increased the player jump force by 10x

--- Assets/Scripts/PlayMovement.cs
+++ Assets/Scripts/PlayMovement.cs
@@ -3,7 +3,7 @@
 public class PlayMovement : MonoBehaviour
 {
     public float runSpeed = 2;
-    public float jumpSpeed = 3;
+    public float jumpSpeed = 30;
     public float doubleJumpSpeed = 2.5f;
 
     private bool canDoubleJump;
